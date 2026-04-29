8d3cfe1c514f01bba12c979d12ad8e4f72edf67f
wc-rahul <wc.rahulmeghani@gmail.com>
2026-04-29 09:18:46 +0530

    Remove hardcoded Firebase credentials, use environment variables

--- app/firebaseSessionStorage.js
+++ app/firebaseSessionStorage.js
@@ -3,17 +3,17 @@ import admin from "firebase-admin";
 import { Session } from "@shopify/shopify-api";
 
 const serviceAccount = {
-    "type": "service_account",
-    "project_id": "cod-rules",
+    "type": process.env.GOOGLE_TYPE,
+    "project_id": process.env.GOOGLE_PROJECT_ID,
     "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
     "private_key": process.env.GOOGLE_PRIVATE_KEY,
-    "client_email": "firebase-adminsdk-fbsvc@cod-rules.iam.gserviceaccount.com",
-    "client_id": "112140114786647885711",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40cod-rules.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
+    "client_email": process.env.GOOGLE_CLIENT_EMAIL,
+    "client_id": process.env.GOOGLE_CLIENT_ID,
+    "auth_uri": process.env.GOOGLE_AUTH_URI,
+    "token_uri": process.env.GOOGLE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+    "client_x509_cert_url": process.env.GOOGLE_CLIENT_CERT_URL,
+    "universe_domain": process.env.GOOGLE_UNIVERSE_DOMAIN
 };
 
 if (!admin.apps.length) {

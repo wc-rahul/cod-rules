d76226291e9f4be4d9db5498e4ce3169386a51ab
wc-rahul <wc.rahulmeghani@gmail.com>
2026-04-29 09:23:37 +0530

    true to

--- app/firebaseSessionStorage.js
+++ app/firebaseSessionStorage.js
@@ -6,7 +6,7 @@ const serviceAccount = {
     "type": process.env.GOOGLE_TYPE,
     "project_id": process.env.GOOGLE_PROJECT_ID,
     "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
-    "private_key": process.env.GOOGLE_PRIVATE_KEY,
+    "private_key": process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     "client_email": process.env.GOOGLE_CLIENT_EMAIL,
     "client_id": process.env.GOOGLE_CLIENT_ID,
     "auth_uri": process.env.GOOGLE_AUTH_URI,
